1d63f0bc088c5b23e7257220c98bf824931bed39
Hamisi Rawlins <hamisirawlins@Hamisis-MacBook-Air.local>
2023-03-29 14:50:01 +0300

    Scaffold Login UI

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -31,166 +31,169 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: Colors.grey[100],
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: size.height * 0.02,
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                width: size.width * 0.3,
+                height: size.height * 0.3,
+              ),
+              Text(
+                'Welcome',
+                style: GoogleFonts.montserrat(
+                  textStyle: TextStyle(fontSize: 28, color: Colors.grey[700]),
                 ),
-                Image.asset(
-                  'assets/images/logo.png',
-                  width: size.width * 0.3,
-                  height: size.height * 0.3,
-                ),
-                Text(
-                  'Welcome',
-                  style: GoogleFonts.montserrat(
-                    textStyle: TextStyle(fontSize: 28, color: Colors.grey[700]),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height * 0.01,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Email",
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(color: kPrimaryColor),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: BorderSide(color: Colors.grey.shade400),
-                        ),
-                        fillColor: Colors.grey[200]),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height * 0.02,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: TextField(
-                    obscureText: true,
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Password",
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(color: kPrimaryColor),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: BorderSide(color: Colors.grey.shade400),
-                        ),
-                        fillColor: Colors.grey[200]),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return ForgotPasswordPage();
-                          }));
-                        },
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.grey[600]),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: signIn,
-                  child: Container(
-                    padding: EdgeInsets.all(18),
-                    margin: EdgeInsets.symmetric(horizontal: 25),
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(35)),
-                    child: const Center(
-                      child: Text(
-                        "Sign In",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              SizedBox(
+                height: size.height * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      filled: true,
+                      hintText: "Email",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(color: kPrimaryColor),
                       ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height * 0.02,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.grey.shade400),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          "Or",
-                          style: TextStyle(color: Colors.grey[900]),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      )
-                    ],
-                  ),
+                      fillColor: Colors.grey[200]),
                 ),
-                Row(
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      filled: true,
+                      hintText: "Password",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(color: kPrimaryColor),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      fillColor: Colors.grey[200]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    const Expanded(child: SizedBox()),
-                    Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: kPrimaryColor),
-                          borderRadius: BorderRadius.circular(45),
-                          color: Colors.grey[200]),
-                      child: Image.asset(
-                        'assets/images/google.png',
-                        height: size.height * 0.05,
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return ForgotPasswordPage();
+                        }));
+                      },
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.grey[600]),
                       ),
-                    ),
-                    const Expanded(child: SizedBox()),
+                    )
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const [
-                      Text('Not a member?'),
-                      Text(
-                        'Register Now',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
+              ),
+              GestureDetector(
+                onTap: signIn,
+                child: Container(
+                  padding: EdgeInsets.all(18),
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(35)),
+                  child: const Center(
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Or",
+                        style: TextStyle(color: Colors.grey[900]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: kPrimaryColor),
+                        borderRadius: BorderRadius.circular(45),
+                        color: Colors.grey[200]),
+                    child: Image.asset(
+                      'assets/images/google.png',
+                      height: size.height * 0.05,
+                    ),
+                  ),
+                  const Expanded(child: SizedBox()),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Expanded(child: SizedBox()),
+                    Text('Not a member?'),
+                    Text(
+                      'Register Now',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(child: SizedBox()),
+                  ],
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
